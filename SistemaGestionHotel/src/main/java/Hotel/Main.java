380c1ff4a0b63f6b8b6e3084ae3e59b93ec98b95
Erik Maximiliano Ovejero <57911902+erik-ovejero@users.noreply.github.com>
2026-06-06 18:03:31 -0300

    Log In Jframe
    Se creó la base para el Log In con Swing en Apache NetBeans

---
+++ SistemaGestionHotel/src/main/java/Hotel/Main.java
@@ -0,0 +1,12 @@
+package Hotel;
+
+/**
+ *
+ * @author Monke
+ */
+public class Main {
+    public static void main(String[] args) {
+    Formularios.LogInForm logInFormulario = new Formularios.LogInForm();
+logInFormulario.setVisible(true);
+}    
+}
