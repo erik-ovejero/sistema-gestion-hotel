78ef80ccbca312bb4dca1ae392221d52f2e43899
Erik Maximiliano Ovejero <57911902+erik-ovejero@users.noreply.github.com>
2026-06-27 22:48:25 -0300

    Calculadora tibia
    calcula el precio de oro a tibiacoins, dólares y pesos

---
+++ CalculadoraTibiaCoinsOro/src/main/java/com/mycompany/calculadoratibiacoinsoro/ConversorOro.java
@@ -0,0 +1,11 @@
+package com.mycompany.calculadoratibiacoinsoro;
+
+public class ConversorOro {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new ConversorOroGUI().setVisible(true);
+        });
+    }
+
+}
